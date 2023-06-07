70f64922042bc979358b888104394c286fa13bbf
lyoun3060 <omega3060@naver.com>
2023-06-07 10:35:25 +0900

    230607 2교시 페이징 라우팅 기초 작업2 NavLink 설정 및 확인

--- class-examples/src/ch16(pdtest)/pages/ItemsPage.js
+++ class-examples/src/ch16(pdtest)/pages/ItemsPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 //URL 매핑 할 때, path 경로의 변수를 동적으로 사용할 예정.
 // 1번 게시글 보고싶다.  호스트서버/board/1
 // 문법 -> board/:동적변수 , 사용
+// 예제         <Route path="/:category" element={<ItemsPage/>}/>
 import { useParams } from 'react-router-dom';
+//현재 공공데이터 작업하는 컴포넌트의 큰 틀 2개(카테고리, 아이템 리스트)
 import Categories from '../Components/Categories';
 import ItemList from '../ItemList';
 
@@ -10,6 +12,7 @@ import ItemList from '../ItemList';
 const ItemsPage = ({match}) => {
     //useParams 를 사용함. 
     // 순서1 , 선언 useParams
+    //:category -> 1)food, 2)walking
     const params = useParams();
     // 순서2, 적용
     //params.category 값이 존재 한다면 해당 값을 사용하고, 만약, 없다면 기본 값으로 : 'food'를 사용함.
